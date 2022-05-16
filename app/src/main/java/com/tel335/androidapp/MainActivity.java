27129fa2584d4ef639c564e5806b456eecc44b5e
Francisco Cabezas <francisco.cabezas@xkivertech.cl>
2022-05-16 19:34:39 -0400

    First Commit

---
+++ app/src/main/java/com/tel335/androidapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tel335.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
