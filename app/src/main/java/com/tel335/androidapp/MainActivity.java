adf2f94c41ff4487022f19d6833898044deda43e
Francisco Cabezas <francisco.cabezas@xkivertech.cl>
2022-05-23 20:10:45 -0400

    (FEATURE): Implements main activity layout. Adds Second Activity

--- app/src/main/java/com/tel335/androidapp/MainActivity.java
+++ app/src/main/java/com/tel335/androidapp/MainActivity.java
@@ -2,13 +2,62 @@ package com.tel335.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nombre;
+    private TextView profesion;
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+
+        nombre = findViewById(R.id.nombre);
+        profesion = findViewById(R.id.profesion);
+        boton = findViewById(R.id.button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
+
+        nombre.setText(R.string.nombre_usuario);
+        profesion.setText(R.string.profesion_usuario);
+
+        boton.setOnClickListener(v -> {
+            Toast.makeText(this, "click!!!", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show();
     }
 }
